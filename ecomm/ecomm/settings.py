99b60ee178e35db385e861fdab276a21ac127425
dnanshmr <dinidiniannisa@gmail.com>
2023-06-06 19:41:15 +0800

    com

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -66,6 +66,8 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
